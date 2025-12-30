cc0771f061ac78faa8ee217a595325272fa337f1
Bambu <byrr113@gmail.com>
2025-12-30 18:08:10 +0900

    [feat: custom 객체 추가]
    custom 객체 MyLabel, IconButton 추가

--- whyRun/whyRun/YerinViewController.swift
+++ whyRun/whyRun/YerinViewController.swift
@@ -8,59 +8,115 @@
 import UIKit
 
 class YerinViewController: UIViewController {
-    
     let profileImage = UIImageView(image: .yerinProfile)
     let petImage = UIImageView(image: .yerinPet)
     
-    let nameLabel = {
-        let label = UILabel()
-        label.text = "변예린"
-        label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 25, weight: .bold)
-        return label
+    let mbtiLabel = MyLabel("INTP", size: 17, weight: .medium)
+    let nameLabel = MyLabel("변예린", size: 25, weight: .bold)
+    let levelLabel = MyLabel("Lv.28", size: 17, weight: .bold)
+    let jobLabel = MyLabel("백수", size: 17, weight: .medium)
+
+    lazy var descriptionLabel: UIStackView = {
+        let stackView = UIStackView()
+        stackView.addArrangedSubview(levelLabel)
+        stackView.addArrangedSubview(jobLabel)
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        return stackView
     }()
+    
+    lazy var labelSet: UIStackView = {
+        let stackView = UIStackView()
+        stackView.addArrangedSubview(mbtiLabel)
+        stackView.addArrangedSubview(nameLabel)
+        stackView.addArrangedSubview(descriptionLabel)
+        stackView.axis = .vertical
+        stackView.spacing = 6
+        stackView.alignment = .center
+        return stackView
+    }()
+
+    let petButton = IconButton(icon: .yerinPetIcon)
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUI()
-        
-        
     }
     
+    //MARK: UI 셋업
     func setUI() {
-        view.backgroundColor = UIColor(red: 0.19, green: 0.22, blue: 0.32, alpha: 1.00)
+        view.backgroundColor = UIColor(red: 0.19, green: 0.22, blue: 0.32, alpha: 1.00) // HEX #303952
         
         view.addSubview(profileImage)
-        view.addSubview(nameLabel)
+        view.addSubview(labelSet)
+        view.addSubview(petButton)
         
-        profileImage.translatesAutoresizingMaskIntoConstraints = false
         profileImage.contentMode = .scaleAspectFit
         
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        profileImage.translatesAutoresizingMaskIntoConstraints = false
+        labelSet.translatesAutoresizingMaskIntoConstraints = false
+        petButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             profileImage.widthAnchor.constraint(equalToConstant: (profileImage.image?.size.width ?? 0) * 1.1),
             profileImage.heightAnchor.constraint(equalToConstant: (profileImage.image?.size.height ?? 0) * 1.1),
-            profileImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -(view.frame.width * 0.3)),
+            profileImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -(view.frame.width * 0.1)),
             profileImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            labelSet.bottomAnchor.constraint(equalTo: profileImage.topAnchor, constant: -20),
+            labelSet.centerXAnchor.constraint(equalTo: profileImage.centerXAnchor, constant: -(profileImage.frame.width / 2 * 0.15)),
+            
+            petButton.widthAnchor.constraint(equalToConstant: 65),
+            petButton.heightAnchor.constraint(equalToConstant: 65),
+            petButton.topAnchor.constraint(equalTo: profileImage.topAnchor, constant: 20),
+            petButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -(view.frame.width * 0.2))
             ])
         
-        NSLayoutConstraint.activate([
-            nameLabel.bottomAnchor.constraint(equalTo: profileImage.topAnchor, constant: -50),
-            nameLabel.centerXAnchor.constraint(equalTo: profileImage.centerXAnchor)
-        ])
-//        profileImage.addSubview(petImage)
-//        
-//        petImage.translatesAutoresizingMaskIntoConstraints = false
-//        petImage.contentMode = .scaleAspectFit
-//        
-//        NSLayoutConstraint.activate([
-//            petImage.widthAnchor.constraint(equalToConstant: (petImage.image?.size.width ?? 0) * 0.4),
-//            petImage.heightAnchor.constraint(equalToConstant: (petImage.image?.size.height ?? 0) * 0.4),
-//            petImage.trailingAnchor.constraint(equalTo: profileImage.leadingAnchor, constant: profileImage.frame.width * 0.3),
-//            petImage.bottomAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: (profileImage.frame.height * 0.05))
-//        ])
+        petButton.addTarget(self, action: #selector(pushPetButton), for: .touchUpInside)
     }
     
+    @objc func pushPetButton() {
+        print("petButton pushed")
+    }
+    
+}
+
+//MARK: 커스텀 객체
+class MyLabel: UILabel {
+    init(_ text: String, size: CGFloat, weight: UIFont.Weight) {
+        super.init(frame: .zero)
+        
+        self.text = text
+        textColor = .white
+        font = UIFont.systemFont(ofSize: size, weight: weight)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class IconButton: UIButton {
+    init(icon: UIImage) {
+        super.init(frame: .zero)
+        
+        clipsToBounds = false
+        layer.cornerRadius = 15
+        backgroundColor = UIColor(red: 0.98, green: 0.60, blue: 0.23, alpha: 1.00)
+        
+        layer.borderWidth = 3
+        layer.borderColor = UIColor(red: 0.96, green: 0.73, blue: 0.23, alpha: 1.00).cgColor // HEX #f6b93b
+        layer.shadowColor = UIColor(red: 1.00, green: 0.98, blue: 0.40, alpha: 1.00).cgColor // HEX #fffa65
+        layer.shadowOpacity = 1
+        layer.shadowOffset = .zero
+        
+        setImage(icon, for: .normal)
+        imageView?.contentMode = .scaleAspectFit
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
