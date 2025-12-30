18bc977f4e2533e4781a99a557854299275cde2c
Bambu <byrr113@gmail.com>
2025-12-30 21:40:13 +0900

    [feat: PillView 생성]
    custom Pill View 클래스 구현

--- whyRun/whyRun/YerinViewController.swift
+++ whyRun/whyRun/YerinViewController.swift
@@ -26,10 +26,7 @@ class YerinViewController: UIViewController {
     }()
     
     lazy var labelSet: UIStackView = {
-        let stackView = UIStackView()
-        stackView.addArrangedSubview(mbtiLabel)
-        stackView.addArrangedSubview(nameLabel)
-        stackView.addArrangedSubview(descriptionLabel)
+        let stackView = UIStackView(arrangedSubviews: [mbtiLabel, nameLabel, descriptionLabel])
         stackView.axis = .vertical
         stackView.spacing = 6
         stackView.alignment = .center
@@ -37,7 +34,36 @@ class YerinViewController: UIViewController {
     }()
 
     let petButton = IconButton(icon: .yerinPetIcon)
-
+    let blogButton = IconButton(icon: .yerinBlogIcon)
+    
+    lazy var buttonSet: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [petButton, blogButton])
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        return stackView
+    }()
+    
+    let characteristics: UIStackView = {
+        let stackView = UIStackView()
+        
+        let first = ["성실함", "친구들의 상담사", "취미부자"].map{ PillView($0) }
+        
+        let labels = [
+            MyLabel("나의 키워드", size: 20, weight: .bold),
+            MyLabel("...", size: 18, weight: .medium),
+            MyLabel("나의 목표", size: 20, weight: .bold),
+            MyLabel("...", size: 18, weight: .medium),
+            MyLabel("한 줄 메시지", size: 20, weight: .bold),
+            MyLabel("...", size: 18, weight: .medium)
+        ]
+        
+        labels.forEach { stackView.addArrangedSubview( $0 ) }
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .trailing
+        
+        return stackView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,19 +73,29 @@ class YerinViewController: UIViewController {
     
     //MARK: UI 셋업
     func setUI() {
+        let a = PillView("성실함")
+        a.translatesAutoresizingMaskIntoConstraints = false
+        
         view.backgroundColor = UIColor(red: 0.19, green: 0.22, blue: 0.32, alpha: 1.00) // HEX #303952
         
         view.addSubview(profileImage)
         view.addSubview(labelSet)
-        view.addSubview(petButton)
+        view.addSubview(buttonSet)
+        view.addSubview(characteristics)
+        view.addSubview(a)
+
         
         profileImage.contentMode = .scaleAspectFit
         
         profileImage.translatesAutoresizingMaskIntoConstraints = false
         labelSet.translatesAutoresizingMaskIntoConstraints = false
         petButton.translatesAutoresizingMaskIntoConstraints = false
+        blogButton.translatesAutoresizingMaskIntoConstraints = false
+        buttonSet.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
+            a.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            
             profileImage.widthAnchor.constraint(equalToConstant: (profileImage.image?.size.width ?? 0) * 1.1),
             profileImage.heightAnchor.constraint(equalToConstant: (profileImage.image?.size.height ?? 0) * 1.1),
             profileImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -(view.frame.width * 0.1)),
@@ -70,8 +106,11 @@ class YerinViewController: UIViewController {
             
             petButton.widthAnchor.constraint(equalToConstant: 65),
             petButton.heightAnchor.constraint(equalToConstant: 65),
-            petButton.topAnchor.constraint(equalTo: profileImage.topAnchor, constant: 20),
-            petButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -(view.frame.width * 0.2))
+            blogButton.widthAnchor.constraint(equalToConstant: 65),
+            blogButton.heightAnchor.constraint(equalToConstant: 65),
+            
+            buttonSet.topAnchor.constraint(equalTo: profileImage.topAnchor, constant: 20),
+            buttonSet.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -(view.frame.width * 0.1))
             ])
         
         petButton.addTarget(self, action: #selector(pushPetButton), for: .touchUpInside)
@@ -91,6 +130,7 @@ class MyLabel: UILabel {
         self.text = text
         textColor = .white
         font = UIFont.systemFont(ofSize: size, weight: weight)
+        textAlignment = .center
     }
     
     required init?(coder: NSCoder) {
@@ -104,7 +144,7 @@ class IconButton: UIButton {
         
         clipsToBounds = false
         layer.cornerRadius = 15
-        backgroundColor = UIColor(red: 0.98, green: 0.60, blue: 0.23, alpha: 1.00)
+        backgroundColor = UIColor(red: 0.97, green: 0.84, blue: 0.58, alpha: 1.00)
         
         layer.borderWidth = 3
         layer.borderColor = UIColor(red: 0.96, green: 0.73, blue: 0.23, alpha: 1.00).cgColor // HEX #f6b93b
@@ -114,6 +154,36 @@ class IconButton: UIButton {
         
         setImage(icon, for: .normal)
         imageView?.contentMode = .scaleAspectFit
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+class PillView: UIView {
+    init(_ text: String) {
+        super.init(frame: .zero)
+        
+        let label = MyLabel(text, size: 18, weight: .bold)
+        
+        backgroundColor = UIColor(white: 1, alpha: 0.3)
+        layer.masksToBounds = true
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            heightAnchor.constraint(equalToConstant: 30),
+            
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
+            label.topAnchor.constraint(equalTo: topAnchor),
+            label.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+        
+        layer.cornerRadius = 15
     }
     
     required init?(coder: NSCoder) {
