fef28a7538c3e84d159f05e0eb292fac622d01e8
hanjuheon <godzx3@gmail.com>
2025-12-31 01:47:33 +0900

    소개 페이지 완성
    # Feat
    - 자기 소개글 추가
    - 학력 및 이력에 대한 상위 제목 추가
    
    # refactor
    -학력 및 이력 배치 변경
    - 개인정보 컨트롤 배치 변경
    - 개인정보에 MBTI항목 추가
    
    # build
    - MBTI이미지  추가

--- whyRun/whyRun/PageHan.swift
+++ whyRun/whyRun/PageHan.swift
@@ -26,7 +26,7 @@ class PageHan: UIViewController {
     // 내용 폰트 설정
     let contentAttribute: [NSAttributedString.Key: Any] =
     [
-        .font: UIFont.boldSystemFont(ofSize: 12),
+        .font: UIFont.boldSystemFont(ofSize: 13),
         .foregroundColor: UIColor.black
     ]
     
@@ -91,7 +91,7 @@ class PageHan: UIViewController {
             contentView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
             contentView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
             contentView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
-            contentView.heightAnchor.constraint(equalToConstant: 2000),
+            contentView.heightAnchor.constraint(equalToConstant: 700),
             bottomcontentVstackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
             bottomcontentVstackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             bottomcontentVstackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
@@ -133,7 +133,7 @@ class PageHan: UIViewController {
             string:"""
             안녕하세요.
             제 이름은 한주헌 입니다.
-            소통하는 개발자가 되고싶어요
+            신뢰받는 개발자가 되고 싶습니다.
             """,
             attributes: [
                 .font: UIFont.boldSystemFont(ofSize: 20),
@@ -144,13 +144,13 @@ class PageHan: UIViewController {
         let strokeAttributes: [NSAttributedString.Key: Any] = [
             .font: UIFont.boldSystemFont(ofSize: 25),
             .strokeColor: UIColor.white,
-            .strokeWidth: -4.0,
-            .foregroundColor: UIColor.clear
+            .strokeWidth: -2.0,
+            .foregroundColor: UIColor.systemMint
         ]
         
         var ChangeRange = (headLineStr.string as NSString).range(of: "한주헌")
         headLineStr.addAttributes(strokeAttributes, range: ChangeRange)
-        ChangeRange = (headLineStr.string as NSString).range(of: "소통하는 개발자")
+        ChangeRange = (headLineStr.string as NSString).range(of: "신뢰받는 개발자")
         headLineStr.addAttributes(strokeAttributes, range: ChangeRange)
         
         headLineLabel.attributedText = headLineStr
@@ -188,15 +188,25 @@ class PageHan: UIViewController {
         // 내용 헤더 컴포넌트 세팅
         let contentHeaderLabel = UILabel()
         contentHeaderLabel.attributedText = NSAttributedString(
-            string: "A B O U T",
+            string: "Introduction",
             attributes: headerAttribute,
         )
         contentHeaderLabel.textAlignment = .center
+
+        // 내용 헤더 컴포넌트 세팅
+        let ProfileHeaderLabel = UILabel()
+        ProfileHeaderLabel.attributedText = NSAttributedString(
+            string: "Developer Profile",
+            attributes: headerAttribute,
+        )
+        ProfileHeaderLabel.textAlignment = .center
+
         
         // 내용 컴포넌트 세팅
         let contentHStackView1 = UIStackView()
         contentHStackView1.axis = .horizontal
         contentHStackView1.translatesAutoresizingMaskIntoConstraints = false
+        contentHStackView1.backgroundColor = .white
         
         let secUIview = UIView()
         secUIview.translatesAutoresizingMaskIntoConstraints = false
@@ -204,9 +214,9 @@ class PageHan: UIViewController {
         let thirdUIview = UIView()
         thirdUIview.translatesAutoresizingMaskIntoConstraints = false
         thirdUIview.backgroundColor = .white
-        
-        
         let infoView = setContenttoinfo()
+
+
         let eduAndWorkView = setContentToEduAndWork()
         let skillView = setContentToSkill()
         
@@ -216,10 +226,16 @@ class PageHan: UIViewController {
         
         mainVSteckView.addArrangedSubview(contentHeaderLabel)
         mainVSteckView.addArrangedSubview(infoView)
+        mainVSteckView.addArrangedSubview(ProfileHeaderLabel)
         mainVSteckView.addArrangedSubview(contentHStackView1)
+        
+        mainVSteckView.setCustomSpacing(20, after: infoView)
+        mainVSteckView.setCustomSpacing(20, after: ProfileHeaderLabel)
+
         
         NSLayoutConstraint.activate([
             infoView.widthAnchor.constraint(equalTo: mainVSteckView.widthAnchor),
+            contentHStackView1.widthAnchor.constraint(equalTo: mainVSteckView.widthAnchor)
            // infoView.centerYAnchor.constraint(equalTo: mainVSteckView.centerYAnchor),
         ])
         
@@ -232,10 +248,17 @@ class PageHan: UIViewController {
         let infoView = UIView()
         infoView.translatesAutoresizingMaskIntoConstraints = false
         infoView.backgroundColor = .white
-        let infoTotalHStackView = UIStackView()
-        infoTotalHStackView.translatesAutoresizingMaskIntoConstraints = false
-        infoTotalHStackView.axis = .horizontal
-        infoTotalHStackView.spacing = 10
+        
+        let infoTotalVStackView = UIStackView()
+        infoTotalVStackView.translatesAutoresizingMaskIntoConstraints = false
+        infoTotalVStackView.axis = .vertical
+        
+        let infoCoreHStackView = UIStackView()
+        infoCoreHStackView.translatesAutoresizingMaskIntoConstraints = false
+        infoCoreHStackView.axis = .horizontal
+        infoCoreHStackView.spacing = 10
+        infoCoreHStackView.layer.borderWidth = 1
+        infoCoreHStackView.layer.borderColor = UIColor.lightGray.cgColor
         
         // 아이콘 및 이름 배치
         let infoNameVStackView = UIStackView()
@@ -265,58 +288,83 @@ class PageHan: UIViewController {
         let addressStackView = UIStackView()
         addressStackView.translatesAutoresizingMaskIntoConstraints = false
         addressStackView.axis = .vertical
-        addressStackView.spacing = 10
+        addressStackView.spacing = 0
         addressStackView.distribution = .fillEqually
+        //addressStackView.layoutMargins = UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0)
+        addressStackView.isLayoutMarginsRelativeArrangement = true
 
+        let mbtiStackView = UIStackView()
+        mbtiStackView.translatesAutoresizingMaskIntoConstraints = false
+        mbtiStackView.axis = .horizontal
+        let mbtiImage = CustomImageView(image: .mbti, xSize:20,ySize: 20)
+        let mbtiLabel = CustomLabel(text: "  I N F P",align: .left,attribute: contentAttribute)
+        
+        mbtiStackView.addArrangedSubview(mbtiImage)
+        mbtiStackView.addArrangedSubview(mbtiLabel)
+        
         let velogStackView = UIStackView()
         velogStackView.translatesAutoresizingMaskIntoConstraints = false
         velogStackView.axis = .horizontal
         let velogimage = CustomImageView(image: .velogIcon, xSize:20,ySize: 20)
-        let velogLabel = CustomLabel(text: "\tvelog.io/@tyr_00",align: .left,attribute: contentAttribute)
+        let velogTextView = CustomLinkTextView(text: "https://velog.io/@tyr_00", fontSize: 16)
         velogStackView.addArrangedSubview(velogimage)
-        velogStackView.addArrangedSubview(velogLabel)
+        velogStackView.addArrangedSubview(velogTextView)
 
         let gitStackView = UIStackView()
         gitStackView.translatesAutoresizingMaskIntoConstraints = false
         gitStackView.axis = .horizontal
         let gitimage = CustomImageView(image: .githubLogo, xSize:20,ySize: 20)
-        let gitLabel = CustomLabel(text: "\tgodzx3-ctrl",align: .left,attribute: contentAttribute)
+        let gitTextView = CustomLinkTextView(text: "https://github.com/godzx3-ctrl", fontSize: 16)
         gitStackView.addArrangedSubview(gitimage)
-        gitStackView.addArrangedSubview(gitLabel)
+        gitStackView.addArrangedSubview(gitTextView)
         
         let emailStackView = UIStackView()
         emailStackView.translatesAutoresizingMaskIntoConstraints = false
         emailStackView.axis = .horizontal
         let emailImage = CustomImageView(image: .icons8Email96, xSize:20,ySize: 20)
-        let emailLabel = CustomLabel(text: "\tgodzx3@gmail.com",align: .left,attribute: contentAttribute)
+        let emailLabel = CustomLabel(text: " godzx3@gmail.com",align: .left,attribute: contentAttribute)
         emailStackView.addArrangedSubview(emailImage)
         emailStackView.addArrangedSubview(emailLabel)
-        
+        addressStackView.addArrangedSubview(mbtiStackView)
+        addressStackView.addArrangedSubview(emailStackView)
         addressStackView.addArrangedSubview(velogStackView)
         addressStackView.addArrangedSubview(gitStackView)
-        addressStackView.addArrangedSubview(emailStackView)
+        infoCoreHStackView.addArrangedSubview(infoNameVStackView)
+        infoCoreHStackView.addArrangedSubview(addressStackView)
+        infoTotalVStackView.addArrangedSubview(infoCoreHStackView)
         
-        infoTotalHStackView.addArrangedSubview(infoNameVStackView)
-        infoTotalHStackView.addArrangedSubview(addressStackView)
+        let introContentVstackView = UIStackView()
+        introContentVstackView.axis = .vertical
+        introContentVstackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let introheaderLabel = CustomLabel(text: " @Aspiring iOS Developer", align: .left,attribute: contentheaderAttribute)
+        let introTextlabel = CustomLabel(text: "안녕하세요, iOS 새싹개발자, 한주헌입니다.\n개발자로 취직하여 다양한 커뮤니티와 협업 프로젝트를 통해 성장해왔습니다.\niOS 개발에 관심이 많았고,이번 캠프를 통해 Swift와 Xcode를 이용한 다양한 앱을 개발을 해보고, 나아가 iOS 개발자로 취직하는것이 목표입니다.", align: .left,attribute: contentAttribute)
+        
+        introContentVstackView.addArrangedSubview(introheaderLabel)
+        introContentVstackView.addArrangedSubview(introTextlabel)
+        introContentVstackView.setCustomSpacing(6, after: introheaderLabel)
+
+        infoTotalVStackView.addArrangedSubview(introContentVstackView)
+
+        infoTotalVStackView.setCustomSpacing(5, after: infoCoreHStackView)
 
         
-//        infoNameVStackView.setCustomSpacing(16, after: nameLabel)
-        infoView.addSubview(infoTotalHStackView)
+        infoView.addSubview(infoTotalVStackView)
 
-        
         NSLayoutConstraint.activate([
             //infoNameVStackView.heightAnchor.constraint(equalTo:infoTotalHStackView.heightAnchor),
-            infoNameVStackView.widthAnchor.constraint(equalToConstant:140),
+            infoNameVStackView.widthAnchor.constraint(equalToConstant:120),
 
-            infoTotalHStackView.centerXAnchor.constraint(equalTo: infoView.centerXAnchor),
-            infoTotalHStackView.centerYAnchor.constraint(equalTo: infoView.centerYAnchor),
-            infoTotalHStackView.widthAnchor.constraint(equalTo: infoView.widthAnchor),
-            infoTotalHStackView.heightAnchor.constraint(equalToConstant: 110),
+            infoCoreHStackView.centerXAnchor.constraint(equalTo: infoView.centerXAnchor),
+            //infoCoreHStackView.centerYAnchor.constraint(equalTo: infoView.centerYAnchor),
+            infoCoreHStackView.widthAnchor.constraint(equalTo: infoView.widthAnchor),
+            infoCoreHStackView.heightAnchor.constraint(equalToConstant: 120),
 
-            infoTotalHStackView.topAnchor.constraint(equalTo:infoTotalHStackView.topAnchor, constant: 15),
-            infoTotalHStackView.bottomAnchor.constraint(equalTo:infoTotalHStackView.bottomAnchor, constant: -15),
             
-            infoView.heightAnchor.constraint(equalToConstant: 120)
+            infoCoreHStackView.topAnchor.constraint(equalTo:infoCoreHStackView.topAnchor, constant: 15),
+            infoCoreHStackView.bottomAnchor.constraint(equalTo:infoCoreHStackView.bottomAnchor, constant: -15),
+            
+            infoView.heightAnchor.constraint(equalToConstant: 230)
         ])
         
         return infoView
@@ -334,7 +382,7 @@ class PageHan: UIViewController {
         let eduLabel = CustomLabel(text: "EDUCATION", align: .center, attribute: contentheaderAttribute)
         let eduContentLabel = CustomLabel(text: "# 서일대학교(졸업)\n- 소프트웨어공학과\n- 소프트웨어공학과(학사)\n", align: .left, attribute: contentAttribute)
         
-        let workLabel = CustomLabel(text: "Work", align: .center, attribute: contentheaderAttribute)
+        let workLabel = CustomLabel(text: "WORK", align: .center, attribute: contentheaderAttribute)
         let workContentLabel = CustomLabel(text: "# (주)로딕스\n2021.11~2025.10:\n \t연구원, 전임연구원 ", align: .left, attribute: contentAttribute)
         
         eduWorkVStackView.addArrangedSubview(eduLabel)
@@ -350,8 +398,8 @@ class PageHan: UIViewController {
         
         NSLayoutConstraint.activate([
             eduWorkVStackView.centerXAnchor.constraint(equalTo: eduWorkView.centerXAnchor),
-            eduWorkVStackView.widthAnchor.constraint(equalToConstant: 130),
-            eduWorkVStackView.heightAnchor.constraint(equalToConstant: 200),
+            eduWorkVStackView.widthAnchor.constraint(equalToConstant: 150),
+            eduWorkVStackView.heightAnchor.constraint(equalToConstant: 190),
             eduWorkView.widthAnchor.constraint(equalToConstant: 170),
             eduWorkView.heightAnchor.constraint(equalToConstant: 300),
         ])
@@ -359,6 +407,7 @@ class PageHan: UIViewController {
         return eduWorkView
     }
     
+    // Profile 내용 뷰 함수
     func setContentToSkill()->UIView{
         let skillView = UIView()
         skillView.translatesAutoresizingMaskIntoConstraints = false
@@ -371,9 +420,9 @@ class PageHan: UIViewController {
         let skillContentLabel = CustomLabel(text: "- C# Winform\n- Devexpress Tool\n- Geoserver\n- GDAL", align: .left, attribute: contentAttribute)
         
         let worScopeLabel = CustomLabel(text: "WORK SCOPE", align: .center, attribute: contentheaderAttribute)
-        let workscopeLabel1 = CustomLabel(text: "• C# WinForms 기반 프론트엔드 개발\n• DevExpress UI 컴포넌트 활용 및 커스터마이징\n• 위성영상 시각화 및 데이터 연계 기능 구현 (GDAL)\n• GeoServer 연동을 통한 공간정보 서비스 활용\n• Swagger 기반 API 연계 및 데이터 처리", align: .left, attribute: contentAttribute)
+        let workscopeLabel1 = CustomLabel(text: "• C# WinForms 기반 프론트엔드 개발\n• DevExpress UI 컴포넌트 활용 및 커스터마이징\n• 위성영상 시각화 및 데이터 연계 기능 구현 (GDAL)\n• GeoServer 연동을 통한 공간정보 서비스 활용\n• Swagger 기반 API 연계 및 데이터 처리", align: .center, attribute: contentAttribute)
 
-        
+         
         skillVStackView.addArrangedSubview(skillLabel)
         skillVStackView.addArrangedSubview(skillContentLabel)
         skillVStackView.addArrangedSubview(worScopeLabel)
@@ -396,7 +445,7 @@ class PageHan: UIViewController {
     
 }
 
-
+// 커스텀 이미지뷰 생성 함수
 func CustomImageView(image:UIImage,xSize:CGFloat,ySize:CGFloat)->UIImageView
 {
     let imageView = UIImageView()
@@ -411,6 +460,7 @@ func CustomImageView(image:UIImage,xSize:CGFloat,ySize:CGFloat)->UIImageView
     return imageView
 }
 
+// 커스텀 레이블 생성 함수
 func CustomLabel(text:String, align: NSTextAlignment,attribute:[NSAttributedString.Key : Any]? = nil)->UILabel{
     let customLabel = UILabel()
     customLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -424,6 +474,17 @@ func CustomLabel(text:String, align: NSTextAlignment,attribute:[NSAttributedStri
     return customLabel
 }
 
+// 커스텀 링크텍스트 생성 함수
+func CustomLinkTextView(text:String, fontSize:CGFloat)->UITextView{
+    let textView = UITextView()
+    textView.text = text
+    textView.font = .systemFont(ofSize: fontSize)
+    textView.textColor = .systemBlue
+    textView.isEditable = false
+    textView.isScrollEnabled = false
+    textView.dataDetectorTypes = .link
+    return textView
+}
 
 #Preview
 {
