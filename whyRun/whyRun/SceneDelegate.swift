67cabbbd32141dd1e089351422d6b0d29f861253
coduhee <vdyjb@ewha.ac.kr>
2025-12-31 17:51:01 +0900

    신델리게이트 컨트롤러 이름 수정

--- whyRun/whyRun/SceneDelegate.swift
+++ whyRun/whyRun/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         window = UIWindow(windowScene: windowScene)
 
-        window?.rootViewController = MemberADetailViewController()
+        window?.rootViewController = MainViewController()
 
         window?.makeKeyAndVisible()
     }
