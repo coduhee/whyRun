d6c08ea88678b06192c6715d89238610cf593bdd
Bambu <byrr113@gmail.com>
2025-12-31 17:51:05 +0900

    🩹chore: sceneDelegate 시뮬레이션 rootController 수정
    YerinViewController() -> MainViewController()

--- whyRun/whyRun/SceneDelegate.swift
+++ whyRun/whyRun/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         window = UIWindow(windowScene: windowScene)
 
-        window?.rootViewController = YerinViewController()
+        window?.rootViewController = MainViewController()
 
         window?.makeKeyAndVisible()
     }
