5c890cb6fdc0e206a672641a441384c720fb6d94
hanjuheon <godzx3@gmail.com>
2025-12-31 17:51:59 +0900

    # chore - SceneDelegate 변경

--- whyRun/whyRun/SceneDelegate.swift
+++ whyRun/whyRun/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         window = UIWindow(windowScene: windowScene)
 
-        window?.rootViewController = PageHan()
+        window?.rootViewController = MainViewController()
 
         window?.makeKeyAndVisible()
     }
