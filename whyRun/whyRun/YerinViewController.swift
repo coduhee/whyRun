7c7000d4d8305b5016f8bc2fb40caa060dc2b5de
Bambu <byrr113@gmail.com>
2025-12-31 14:13:45 +0900

    ✨feat: 반려동물 정보 카드 구현
    petButton 클릭 시 노출될 반려동물 정보 카드 구현
     - 관련 VC 생성

--- whyRun/whyRun/YerinViewController.swift
+++ whyRun/whyRun/YerinViewController.swift
@@ -191,6 +191,7 @@ class YerinViewController: UIViewController {
         ])
     }
     
+    
 
     
     //MARK: 버튼 기능 구현
@@ -209,11 +210,10 @@ class YerinViewController: UIViewController {
     
     // 펫 버튼 액션
     @objc func petButtonPushed() {
-        let alert = UIAlertController(title: "펫: 짜코", message: "제가 기르는 도마뱀 짜코입니다.", preferredStyle: .alert)
-        let close = UIAlertAction(title: "닫기", style: .default, handler: nil)
-        
-        alert.addAction(close)
-        present(alert, animated: true, completion: nil)
+        let vc = PetCardViewController()
+        vc.modalPresentationStyle = .overFullScreen
+        vc.modalTransitionStyle = .crossDissolve
+        present(vc, animated: true)
     }
     
 }
@@ -298,5 +298,152 @@ class PillView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// 반려동물 카드 VC
+final class PetCardViewController: UIViewController {
+    
+    // 배경 반투명 뷰
+   let dimView: UIView = {
+        let view = UIView()
+       view.backgroundColor = .black.withAlphaComponent(0.5)
+        return view
+    }()
+    
+    // 카드 콘텐츠 배경 뷰
+    private let cardView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 15
+        view.clipsToBounds = true // 카드 밖으로 컨텐츠가 나갔을 때 카드 모양대로 자르기 위한 제한
+        
+        view.layer.borderWidth = 5
+        view.layer.borderColor = UIColor(red: 0.96, green: 0.73, blue: 0.23, alpha: 1.00).cgColor // HEX #f6b93b
+
+        return view
+    }()
+    
+    private let shadowView: UIView = {
+        let view = UIView()
+        view.layer.shadowColor = UIColor(red: 1.00, green: 0.98, blue: 0.40, alpha: 1.00).cgColor // HEX #fffa65
+        view.layer.shadowOpacity = 1
+        view.layer.shadowRadius = 10
+        view.layer.shadowOffset = .zero
+        return view
+    }()
+    
+    // 콘텐츠
+    private let content: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayout()
+        setContent()
+        setDismiss()
+    }
+    
+    func setupLayout() {
+        view.addSubview(dimView)
+        dimView.frame = view.bounds
+
+        view.addSubview(shadowView)
+        shadowView.translatesAutoresizingMaskIntoConstraints = false
+        
+        shadowView.addSubview(cardView)
+        cardView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            shadowView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            shadowView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            shadowView.widthAnchor.constraint(equalToConstant: 280),
+            
+            cardView.topAnchor.constraint(equalTo: shadowView.topAnchor),
+            cardView.bottomAnchor.constraint(equalTo: shadowView.bottomAnchor),
+            cardView.leadingAnchor.constraint(equalTo: shadowView.leadingAnchor),
+            cardView.trailingAnchor.constraint(equalTo: shadowView.trailingAnchor)
+        ])
+        // height는 콘텐츠 길이에 따라 가변
+    }
+
+    func setContent() {
+        setContentUI()
+        cardView.addSubview(content)
+        content.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            content.topAnchor.constraint(equalTo: cardView.topAnchor, constant: 20),
+            content.bottomAnchor.constraint(equalTo: cardView.bottomAnchor, constant: -20),
+            content.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 20),
+            content.trailingAnchor.constraint(equalTo: cardView.trailingAnchor, constant: -20)
+            ])
+    }
+    
+    
+    func setContentUI() {
+        let img = UIImageView(image: .yerinPet)
+        
+        let name = MyLabel("짜코", size: 24, weight: .bold, color: .black)
+        let lv = MyLabel("Lv.1 크레스티드 게코", size: 19, weight: .medium, color: .black)
+        let fav: UIStackView = {
+            let title = MyLabel("좋아하는 것:", size: 19, weight: .bold, color: .black)
+            let ans = MyLabel("사료, 홍시", size: 19, weight: .medium, color: .black)
+            
+            let stackView = UIStackView(arrangedSubviews: [title, ans])
+            stackView.axis = .horizontal
+            stackView.spacing = 6
+            return stackView
+        }()
+        let hate: UIStackView = {
+            let title = MyLabel("싫어하는 것:", size: 19, weight: .bold, color: .black)
+            let ans = MyLabel("사람", size: 19, weight: .medium, color: .black)
+            
+            let stackView = UIStackView(arrangedSubviews: [title, ans])
+            stackView.axis = .horizontal
+            stackView.spacing = 6
+            return stackView
+        }()
+
+        let texts: UIStackView = {
+            let stackView = UIStackView(arrangedSubviews: [name, lv, fav, hate])
+            stackView.axis = .vertical
+            stackView.spacing = 10
+            stackView.alignment = .center
+            return stackView
+        }()
+        
+        content.addSubview(img)
+        content.addSubview(texts)
+        
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.contentMode = .scaleAspectFit
+        
+        texts.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            img.widthAnchor.constraint(equalToConstant: 150),
+            img.heightAnchor.constraint(equalToConstant: 180),
+            img.centerXAnchor.constraint(equalTo: content.centerXAnchor),
+            img.topAnchor.constraint(equalTo: content.topAnchor),
+            
+            texts.centerXAnchor.constraint(equalTo: content.centerXAnchor),
+            texts.topAnchor.constraint(equalTo: img.bottomAnchor),
+            texts.leadingAnchor.constraint(equalTo: content.leadingAnchor),
+            texts.trailingAnchor.constraint(equalTo: content.trailingAnchor),
+            texts.bottomAnchor.constraint(equalTo: content.bottomAnchor)
+        ])
+    }
+    
+    func setDismiss() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(close))
+        dimView.addGestureRecognizer(tap)
+    }
+    
+    @objc func close() {
+        dismiss(animated: true)
     }
 }
